c2d6ec7fbe09d89defe634fcfb88c22e1537b656
Gertjan Gielen <gjgielen@gmail.com>
2021-02-07 21:39:08 +0100

    test

---
+++ src/GymCompanion.Models/Commands/RemoveDeviceCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GymCompanion.Models.Commands
+{
+    public class RemoveDeviceCommand : ICommand
+    {
+        public RemoveDeviceCommand(string id)
+        {
+            Id = id;
+        }
+
+        public string Id { get;}
+    }
+}
